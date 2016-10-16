c1d77244ae40dfc7a7b2da1eea25eda915243a1a
Андрей <@ANDREYPC>
2016-10-16 23:22:05 +0300

    Add class code writer

---
+++ DTOGenerator/DTOGenerator/ClassWriter.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace DTOGenerator
+{
+    internal class ClassWriter
+    {
+        private readonly string _outputDirectoryPath;
+
+        internal ClassWriter(string outputDirectoryPath)
+        {
+            _outputDirectoryPath = outputDirectoryPath;
+            Directory.CreateDirectory(outputDirectoryPath);
+        }
+
+        internal void Write(string className, string classCode)
+        {
+            var dtoFileName = Path.Combine(_outputDirectoryPath, className + ".cs");
+            using (var streamWriter = new StreamWriter(dtoFileName))
+            {
+                streamWriter.Write(classCode);
+            }
+        }
+    }
+}
