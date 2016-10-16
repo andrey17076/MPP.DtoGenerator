9cff5bd43c4f7949d1bfcf0d292ffb25b3057ed7
Андрей <@ANDREYPC>
2016-10-16 17:23:57 +0300

    Add dto generation testing project

---
+++ DTOGenerator/DTOGenerator/GenerationConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System.Configuration;
+
+namespace DTOGenerator
+{
+    internal static class GenerationConfiguration
+    {
+        internal static int TaskCount => int.Parse(ConfigurationManager.AppSettings["taskCount"]);
+        internal static string NameSpaceName => ConfigurationManager.AppSettings["namespaceName"];
+    }
+}
