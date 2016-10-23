bf96d62ba9e3fca6e0f4878120e5cc08885342b7
Андрей <@ANDREYPC>
2016-10-24 00:15:36 +0300

    Add plugin samples

---
+++ DTOGenerator/TypeDescriptionPlugins/ULongTypeDescriptionPlugin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using GenerationLibrary.TypeAssistance;
+
+namespace TypeDescriptionPlugins
+{
+    public class ULongTypeDescriptionPlugin : ITypeDescription
+    {
+        public string TypeName { get; set; } = "integer";
+        public string FormatName { get; set; } = "uint64";
+        public Type NetType { get; set; } = typeof(ulong);
+    }
+}
