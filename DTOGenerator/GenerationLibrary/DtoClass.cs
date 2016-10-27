5f9e00c70651560e0b9dc939b954c3c2ee4c20f0
Андрей <@ANDREYPC>
2016-10-27 03:01:16 +0300

    Move DtoClass to generating library

---
+++ DTOGenerator/GenerationLibrary/DtoClass.cs
@@ -0,0 +1,19 @@
+﻿namespace GenerationLibrary
+{
+    public class DtoClass
+    {
+
+        public DtoClass(string name)
+        {
+            FileName = name + ".cs";
+        }
+
+        public DtoClass(string name, string code) : this(name)
+        {
+            Code = code;
+        }
+
+        public string FileName { get; private set; }
+        public string Code { get; private set; }
+    }
+}
